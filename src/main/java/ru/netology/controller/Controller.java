1e35abd54101dd77caa54a79266b96a0ad901975
Efimov-Alex <fastic2018l@yandex.ru>
2023-12-22 17:28:08 +0300

    jpa-repository 2

--- src/main/java/ru/netology/controller/Controller.java
+++ src/main/java/ru/netology/controller/Controller.java
@@ -20,16 +20,17 @@ public class Controller {
 
 
     @GetMapping("/by-city")
-    protected List<PERSONS> findPersonByCityOfLiving(@RequestParam (value = "city", required = false) String city) {
+    protected List<PERSONS> findPersonByCityOfLiving(@RequestParam(value = "city", required = false) String city) {
         return repository.findPersonByCityOfLiving(city);
     }
+
     @GetMapping("/by-age")
-    protected List<PERSONS> findPersonByContact_AgeLessThanOrderByContactAsc(@RequestParam(value = "age", required = false) Integer age){
+    protected List<PERSONS> findPersonByContact_AgeLessThanOrderByContactAsc(@RequestParam(value = "age", required = false) Integer age) {
         return repository.findPersonByContact_AgeLessThanOrderByContactAsc(age);
     }
 
     @GetMapping("/by-name&surname")
-    protected Optional<PERSONS> findPersonByContact_NameAndContact_Surname(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname){
+    protected Optional<PERSONS> findPersonByContact_NameAndContact_Surname(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname) {
         return repository.findPersonByContact_NameAndContact_Surname(name, surname);
     }
 
