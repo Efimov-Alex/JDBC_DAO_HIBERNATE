21d9f2f2d0efe7661176c92214834f8d58d41122
Efimov-Alex <fastic2018l@yandex.ru>
2023-12-22 17:27:52 +0300

    jpa-repository 1

--- src/main/java/ru/netology/controller/Controller.java
+++ src/main/java/ru/netology/controller/Controller.java
@@ -1,12 +1,40 @@
 package ru.netology.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.netology.model.PERSONS;
 import ru.netology.repository.Repos;
 
 import java.util.List;
+import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:8080")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/persons")
+public class Controller {
+
+    @Autowired
+    private final Repos repository;
+
+
+    @GetMapping("/by-city")
+    protected List<PERSONS> findPersonByCityOfLiving(@RequestParam (value = "city", required = false) String city) {
+        return repository.findPersonByCityOfLiving(city);
+    }
+    @GetMapping("/by-age")
+    protected List<PERSONS> findPersonByContact_AgeLessThanOrderByContactAsc(@RequestParam(value = "age", required = false) Integer age){
+        return repository.findPersonByContact_AgeLessThanOrderByContactAsc(age);
+    }
+
+    @GetMapping("/by-name&surname")
+    protected Optional<PERSONS> findPersonByContact_NameAndContact_Surname(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname){
+        return repository.findPersonByContact_NameAndContact_Surname(name, surname);
+    }
+
+}
+/*
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/persons")
@@ -22,3 +50,7 @@ public class Controller {
 }
 
 // http://localhost:8080/persons/by-city?city=Paris
+// http://localhost:8080/persons/by-age?age=21
+// http://localhost:8080/persons/by-name&surname?name=alex&surname=smirnov
+
+ */
--- src/main/java/ru/netology/repository/Repos.java
+++ src/main/java/ru/netology/repository/Repos.java
@@ -3,11 +3,24 @@ package ru.netology.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.netology.model.Contact;
 import ru.netology.model.PERSONS;
 
 import java.util.List;
+import java.util.Optional;
 
+public interface Repos extends JpaRepository<PERSONS, Contact> {
+
+    List<PERSONS> findPersonByCityOfLiving(String city);
+
+    List<PERSONS> findPersonByContact_AgeLessThanOrderByContactAsc(Integer age);
+
+    Optional<PERSONS> findPersonByContact_NameAndContact_Surname(String name, String surname);
+
+}
+/*
 @Repository
 @AllArgsConstructor
 public class Repos {
@@ -24,3 +37,5 @@ public class Repos {
 
 
 }
+
+ */
